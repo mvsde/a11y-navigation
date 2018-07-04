97acd944092c516ed434849f96ae3b7cb583351a
Fynn Becker <info@fynn-becker.de>
2018-07-04 19:28:20 +0200

    Build both minified and non-minified bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,45 @@
 import babel from 'rollup-plugin-babel'
 import eslint from 'rollup-plugin-eslint'
+import filesize from 'rollup-plugin-filesize'
+import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import { terser } from 'rollup-plugin-terser'
+
+const ext = process.env.MINIFY ? 'min.js' : 'js'
 
 const config = {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/a11y-navigation.common.js',
+      file: `dist/a11y-navigation.common.${ext}`,
       format: 'cjs'
     },
     {
-      file: 'dist/a11y-navigation.esm.js',
+      file: `dist/a11y-navigation.esm.${ext}`,
       format: 'es'
     },
     {
-      file: 'dist/a11y-navigation.js',
+      file: `dist/a11y-navigation.${ext}`,
       format: 'umd',
       name: 'A11yNavigation'
     }
   ],
   plugins: [
     eslint(),
-    babel({
-      exclude: 'node_modules/**'
-    })
+    babel({ exclude: 'node_modules/**' })
   ]
 }
 
 if (process.env.NODE_ENV === 'development') {
   config.plugins.push(serve())
+  config.plugins.push(livereload())
 }
 
 if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(filesize())
+}
+
+if (process.env.MINIFY) {
   config.plugins.push(terser())
 }
 
